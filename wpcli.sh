a714213db374f16ed35e2e4a350981ae16116b01
Sean Tilbury <sean@tilbury.org>
2019-08-20 11:37:45 +0100

    initial
    shell script

---
+++ wpcli.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set path to wpcli ***check PHP version*** #
+wp="/usr/bin/php-5.6-cli ./wp-cli.phar"
+
+# update all plugins #
+wp plugin update --all
+
+# update all themes #
+wp theme update --all
+
+# update WordPress core #
+wp core update
+
+# verify core files against checksums #
+wp core verify-checksums
+
+# update wpcli to latest version #
+wp cli update --yes
