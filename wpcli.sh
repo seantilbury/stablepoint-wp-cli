529666bdd7b85beb4a6f6c018213291f0ed5e29e
Sean Tilbury <sean@tilbury.org>
2019-08-21 08:42:36 +0100

    add time/date to logs

--- wpcli.sh
+++ wpcli.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#Set alais of path to WP-CLI 
+# set path to wpcli ***check PHP version*** #
 shopt -s expand_aliases
-alias wp="/opt/alt/php72/usr/bin/php /home3/current-cp-user/wp-cli.phar"
+alias wp="/opt/alt/php72/usr/bin/php /home3/bruceatfieldco/wp-cli.phar"
+
+# echo start time/date for logs #
+now="$(date)"
+printf "Updates Started at %s\n" "$now"
 
 # update all plugins #
 wp plugin update --all
@@ -18,3 +22,7 @@ wp core verify-checksums
 
 # update wpcli to latest version #
 wp cli update --yes
+
+# echo finish time/date for logs #
+now="$(date)"
+printf "Updates Completed at %s\n" "$now"
